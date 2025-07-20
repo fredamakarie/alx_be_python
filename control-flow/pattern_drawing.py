02deb764816dbe13dbad27427e7fe13af72dd38d
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-07-20 17:12:06 +0100

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,6 +2,14 @@ pattern_size = int(input("Enter the size of the pattern: "))
 counter  = 0
 
 #for i in range(pattern_size):
-while counter < pattern_size:
-    print (pattern_size * "*", end="\n")
+#while counter < pattern_size:
+    #print (pattern_size * "*", end="\n")
+    #counter += 1
+
+while counter < pattern_size: 
+    j=0
+    while j < pattern_size:
+        print ("*", end="")
+        j+=1
+    print()
     counter += 1
