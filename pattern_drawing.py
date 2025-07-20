b894ba7099a8e2c1ab6460bc27067701ef47f372
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-07-20 16:50:49 +0100

    updates

---
+++ pattern_drawing.py
@@ -0,0 +1,7 @@
+pattern_size = int(input("Enter the size of the pattern: "))
+counter  = 0
+
+#for i in range(pattern_size):
+while counter < pattern_size:
+    print (pattern_size * "*", end="\n")
+    counter += 1
