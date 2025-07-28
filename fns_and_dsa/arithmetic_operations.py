39f24c68bc447e8f518a00883398b1dcdf7e0502
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-07-28 20:49:42 +0100

    updated exercise

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,17 @@
 def perform_operation(num1, num2, operation):
     if operation == "add":
-        num1 + num2
+        result = num1 + num2
     elif operation == "subtract":
-        num1 - num2
+        result = num1 - num2
     elif operation == "add":
-        num1 * num2
+        result = num1 * num2
     elif operation == "add":
-        num1 / num2
+        result = num1 / num2
     elif num2 == 0:
         print("Cannot divide by zero")
     else:
         print("These values are not valid")
+        return result
     # match operation:
     #     case "add":
     #         num1 + num2
