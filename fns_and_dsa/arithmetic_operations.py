627f66aadabf621c044ba1983a9fd0ab015d505a
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-07-28 20:35:29 +0100

    python exercises

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,25 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            num1 + num2
-        case "subtract":
-            num1 - num2
-        case "multiply":
-            num1 * num2
-        case "divide":
-            num1 / num2
-        case _:
-            print("Cannot divide by zero")
+    if operation == "add":
+        num1 + num2
+    elif operation == "subtract":
+        num1 - num2
+    elif operation == "add":
+        num1 * num2
+    elif operation == "add":
+        num1 / num2
+    elif num2 == 0 :
+        print("Cannot divide by zero")
+    else:
+        print("These values are not valid")
+    # match operation:
+    #     case "add":
+    #         num1 + num2
+    #     case "subtract":
+    #         num1 - num2
+    #     case "multiply":
+    #         num1 * num2
+    #     case "divide":
+    #         num1 / num2
+    #     case _:
+    #         print("Cannot divide by zero")
 
--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,15 +1,23 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
-CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 
-def convert_to_celsius(fahrenheit) ():
+def convert_to_celsius(fahrenheit):
     C = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
-convert_to_celsius(fahrenheit) ()
+convert_to_celsius()
 
-def convert_to_fahrenheit(celsius) ():
+def convert_to_fahrenheit(celsius):
     F = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-convert_to_fahrenheit(celsius) ()
+convert_to_fahrenheit()
 
-temperature = input("Enter the temperature to convert: ")
-"Is this temperature in Celsius or Fahrenheit? (C/F): "
+temperature = float(input("Enter the temperature to convert: "))
+unit = "Is this temperature in Celsius or Fahrenheit? (C/F): "
+if unit == "C":
+    celsius = temperature
+    print(f"{celsius}°C is {convert_to_celsius(celsius)}")
+if unit == "F":
+    fahrenheit = temperature
+    print(f"{fahrenheit}°F is {convert_to_celsius(fahrenheit)}°C")
+else:
+    print("Please try again")
