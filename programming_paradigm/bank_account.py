950353a28450986c4070ee81e9ecc616e895af4e
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-08-03 18:10:46 +0100

    updates to python exercises

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,15 +3,17 @@ class BankAccount:
         self.account_balance = account_balance
 
         def deposit(self, amount):
-          self.account_balance += amount
-          return f"Current Balance: {self.account_balance}"
+            self.account_balance += amount
+            return f"Deposited: ${amount}"
          
 
         def withdraw(self, amount):
             if self.account_balance >= amount:
-                self.account_balance -= amount
+             
+                return f"Withdrew: ${amount}"
+
             else:
                 return "Insufficient funds."
 
         def display_balance(self):
-            return f"${self.account_balance}"
+            return f"Current Balance: ${self.account_balance}"
--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,11 +1,17 @@
 def safe_divide(numerator, denominator):
     try:
-        result = float(numerator)/float(denominator)
+        numerator = float(numerator)
+        denominator = float(denominator)
+
+        if denominator == 0:
+            raise ZeroDivisionError ("Error: Cannot divide by zero.")
+    
+        result = numerator/denominator
         return  f"The result of the division is {result}"
     
     except ValueError:
         return "Error: Please enter numeric values only."
    
-    finally: 
-        denominator == 0
-        raise ZeroDivisionError ("Error: Cannot divide by zero.")
+    
+       
+    
