5ae7329a776b57bfb2914cccc75337823c5af3ed
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-08-03 19:52:53 +0100

    update

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,15 +6,12 @@ class BankAccount:
         self.account_balance += amount
         return f"Deposited: ${amount}"
 
-
     def withdraw(self, amount):
         if self.account_balance >= amount:
-            self.account_balance -= amount     
+            self.account_balance -= amount
             return f"Withdrew: ${amount}"
-
         else:
             return "Insufficient funds."
 
-
     def display_balance(self):
-            return f"Current Balance: ${self.account_balance}"
+        return f"Current Balance: ${self.account_balance}"
