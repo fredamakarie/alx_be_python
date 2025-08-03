d9ea2a5a735af771f21c823f5aaea18a2dfe1ba2
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-08-03 18:57:57 +0100

    indentation fixes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,19 +2,19 @@ class BankAccount:
     def __init__(self, account_balance = 0):
         self.account_balance = account_balance
 
-def deposit(self, amount):
-    self.account_balance += amount
-    return f"Deposited: ${amount}"
+    def deposit(self, amount):
+        self.account_balance += amount
+        return f"Deposited: ${amount}"
 
 
-def withdraw(self, amount):
-    if self.account_balance >= amount:
-        self.account_balance -= amount     
-        return f"Withdrew: ${amount}"
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount     
+            return f"Withdrew: ${amount}"
 
-    else:
-        return "Insufficient funds."
+        else:
+            return "Insufficient funds."
 
 
-def display_balance(self):
-        return f"Current Balance: ${self.account_balance}"
+    def display_balance(self):
+            return f"Current Balance: ${self.account_balance}"
