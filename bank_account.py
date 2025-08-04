d0be6043fe37ad1c63b0a66677164f79abf247e1
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-08-04 02:36:00 +0100

    update

---
+++ bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+        return f"Deposited: ${amount}"
+
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return f"Withdrew: ${amount}"
+        elif self.account_balance < amount and amount>0:
+            return "Insufficient funds."
+
+    def display_balance(self):
+        return f"Current Balance: ${self.account_balance}"
