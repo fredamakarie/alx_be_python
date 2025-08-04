cb8f5ea9f39b7cd8612c6b71a898ea1783295007
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-08-04 03:27:10 +0100

    converted account balance to float

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,4 +14,5 @@ class BankAccount:
             return False
 
     def display_balance(self):
+       self.account_balance = float(self.account_balance)
        print (f"Current Balance: ${self.account_balance}")
