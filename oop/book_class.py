3503137de91c7d1146f4f59c48de580785c9390c
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-08-13 18:45:04 +0100

    changed print to return

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,13 +5,13 @@ class Book:
         self.year = year
 
     def __str__(self):
-        print(f"{self.title} by {self.author}, published in {self.year}")
+        return(f"{self.title} by {self.author}, published in {self.year}")
     
     def __repr__(self):
-        print(f"Book('{self.title}', '{self.author}', {self.year})")
+        return(f"Book('{self.title}', '{self.author}', {self.year})")
 
 
     def __del__(self):
         del self.title 
-        print(f"Deleting {self.title}")
+        return(f"Deleting {self.title}")
 
