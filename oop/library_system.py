5863c62fe87d39c6bd94206478d5c341ff833acb
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-08-13 19:05:31 +0100

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,21 +3,21 @@ class Book:
         self.title = title
         self.author = author
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = int(file_size)
     def __str__(self):
-        return f"{self.title} by {self.author}, File Size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = int(page_count)
     def __str__(self):
-        return f"{self.title} by {self.author}, Page Count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
@@ -32,5 +32,6 @@ class Library:
     def list_books(self):
         """Return a list of all books in string form."""
         return [str(book) for book in self.books]
+
 
    
